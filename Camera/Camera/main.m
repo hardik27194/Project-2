624f7d740d5fff52e8d89eee31db285510228117
hardik27194 <hardik.leewayinfotech@gmail.com>
2017-05-10 10:29:13 +0530

    Update main.m

--- Camera/Camera/main.m
+++ Camera/Camera/main.m
@@ -5,6 +5,10 @@
 //  Created by 刘硕 on 16/5/27.
 //  Copyright © 2016年 刘硕. All rights reserved.
 //
+/*
+https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwityfPI4sbTAhVKgI8KHemxAL4QFggiMAA&url=http%3A%2F%2Fstackoverflow.com%2Fquestions%2F13766268%2F3d-text-effect-in-ios&usg=AFQjCNHGjxLnXvJ77jyCULpkEM2qTtK02A&sig2=qPf-noFpPLR3TKoK8BwgdA
+Ana-Maria Stoica
+*/
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
